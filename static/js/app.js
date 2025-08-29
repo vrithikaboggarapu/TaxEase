8d80e43a99df300feeaf4dff27c346cc205c1a73
SrinayanaMandalapu <mandalapunayana@gmail.com>
2025-08-29 23:22:51 +0530

    added close menu on link click feature

--- static/js/app.js
+++ static/js/app.js
@@ -1,9 +1,20 @@
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const body = document.querySelector("body");
+const navLinks = navMenu.querySelectorAll("li");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
   body.classList.toggle("no-scroll");
+});
+
+function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  body.classList.remove("no-scroll");
+}
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
 });
